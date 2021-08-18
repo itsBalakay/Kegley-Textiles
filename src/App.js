2dc9a3a028dea9e956d51433b2dc6eb6faa54f37
Blake Stillwell <bstillwell13@yahoo.com>
2021-08-17 21:13:32 -0400

    updated data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import { Switch, Link, Route } from "react-router-dom";
+
+import Home from "./Components/Home";
+import Header from "./Components/Header";
+import About from "./Components/About";
+// import Contact from "./Components/Contact";
 
 function App() {
+  const [aboutData, setAboutData] = useState({});
+
+  useEffect(() => {
+    fetch("/aboutData.json")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setAboutData(data);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+
+      <Switch>
+        <Route exact path="/" render={(props) => <Home props={aboutData} />} />
+
+        <Route
+          exact
+          path="/AboutUs"
+          render={(props) => <About data={aboutData.main} />}
+        />
+      </Switch>
     </div>
   );
 }
